df5721415c39e57e4b19101629be15a56cde5612
Terney50 <professorcze@gmail.com>
2020-06-09 13:25:19 +0200

    DB created, connected and tested

--- src/ConnectionManager.java
+++ src/ConnectionManager.java
@@ -9,11 +9,11 @@ public class ConnectionManager {
 
         // JDBC driver name and database URL
         static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-        static final String DB_URL = "jdbc:mysql://localhost/";
+        static final String DB_URL = "jdbc:mysql://localhost/?serverTimezone=CET";
 
         //  Database credentials
-        static final String USR = "username";
-        static final String PSW = "password";
+        static final String USR = "User1";
+        static final String PSW = "Passw0rd";
 
     public static void main(String[] args) {
         //System.out.println("Empty...");
@@ -25,16 +25,16 @@ public class ConnectionManager {
             Class.forName("com.mysql.jdbc.Driver");
 
             // STEP 3: Open a connection
-            System.out.println("Connectiong to database...");
+            System.out.println("Connecting to database No.1...");
             conn = DriverManager.getConnection(DB_URL, USR, PSW);
 
-            // STEP 4: Execute a query
+            /*// STEP 4: Execute a query
             System.out.println("Creating database...");
             stmt = conn.createStatement();
 
-            String sql = "CREATE DATABASE STUDENTS";
+            String sql = "CREATE DATABASE BLAH"; // BLAH = DB
             stmt.executeUpdate(sql);
-            System.out.println("Database created successfully...");
+            System.out.println("Database created successfully...");*/
         } catch (SQLException se) {
             // Handle errors for JDBC
             se.printStackTrace();
@@ -46,16 +46,18 @@ public class ConnectionManager {
             try {
                 if(stmt != null)
                     stmt.close();
+                    System.out.println("stmt close...");
             } catch (SQLException se2) {
             }// Nothing we can do
             try {
                 if(conn != null)
                     conn.close();
+                    System.out.println("conn close...");
             } catch (SQLException se) {
                 se.printStackTrace();
             }// End finally try
         }// End try
-        System.out.println("Goodbye!");
+        System.out.println("Connection to database No.1 tested");
     }// End main
 
 
