22c950c94d3c481e763bdb20868130ff4af8b615
Terney50 <professorcze@gmail.com>
2020-06-11 11:14:20 +0200

    blah

--- src/main/java/ConnectionManager.java
+++ src/main/java/ConnectionManager.java
@@ -1,111 +1,208 @@
-//STEP 1. Import required packages
+package main.java;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
 
 public class ConnectionManager {
 
-        // JDBC driver name and database URL
-        static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-        static final String DB_URL = "jdbc:mysql://localhost:3306/?serverTimezone=CET"; // S1
-        static final String DB_URL_S2 = "jdbc:mysql://localhost:3307/?serverTimezone=CET"; // S2
+    private String databaseUrl;
+    private String userName;
+    private String password;
+    private int maxPoolSize = 10;
+    private int connNum = 0;
 
-        //  Database credentials
-        static final String USR = "root"; // S1
-        static final String PSW = "Passw0rd"; // S1
-        static final String USR_S2 = "root"; // S2
-        static final String PSW_S2 = "Passw0rd"; // S2
+    private static final String SQL_VERIFYCONN = "select 1";
 
+    Stack<Connection> freePool = new Stack<>();
+    Set<Connection> occupiedPool = new HashSet<>();
 
-    public static void main(String[] args) {
-        //System.out.println("Empty...");
+    /**
+     * Constructor
+     *
+     * @param databaseUrl
+     *            The connection url
+     * @param userName
+     *            user name
+     * @param password
+     *            password
+     * @param maxSize
+     *            max size of the connection pool
+     */
+    public ConnectionManager(String databaseUrl, String userName, String password, int maxSize) {
+        this.databaseUrl = databaseUrl;
+        this.userName = userName;
+        this.password = password;
+        this.maxPoolSize = maxSize;
+    }
 
-        // S1
+    /**
+     * Get an available connection
+     *
+     * @return An available connection
+     * @throws SQLException
+     *             Fail to get an available connection
+     */
+    public synchronized Connection getConnection() throws SQLException {
         Connection conn = null;
-        Statement stmt = null;
+
+        if (isFull()) {
+            throw new SQLException("The connection pool is full.");
+        }
+
+        conn = getConnectionFromPool();
+
+        // If there is no free connection, create a new one.
+        if (conn == null) {
+            conn = createNewConnectionForPool();
+        }
+
+        /*
+        If there is no action on one connection for some
+        time, the connection is lost. By this, make sure the connection is
+        active. Otherwise reconnect it.
+        */
+        conn = makeAvailable(conn);
+        return conn;
+    }
+
+    /**
+     * Return a connection to the pool
+     *
+     * @param conn
+     *            The connection
+     * @throws SQLException
+     *             When the connection is returned already or it isn't gotten
+     *             from the pool.
+     */
+    public synchronized void returnConnection (Connection conn) throws SQLException {
+        if (conn == null) {
+            throw new NullPointerException();
+        }
+        if (!occupiedPool.remove(conn)) {
+            throw new SQLException("The connection is returned already or it isn't for this pool");
+        }
+        freePool.push(conn);
+    }
+
+    /**
+     * Verify if the connection is full.
+     *
+     * @return if the connection is full
+     */
+    private synchronized boolean isFull() {
+        return ((freePool.size() == 0) && (connNum >= maxPoolSize));
+    }
+
+    /**
+     * Create a connection for the pool
+     *
+     * @return the new created connection
+     * @throws SQLException
+     *             When fail to create a new connection.
+     */
+    private Connection createNewConnectionForPool() throws SQLException {
+        Connection conn = createNewConnection();
+        connNum++;
+        occupiedPool.add(conn);
+        return conn;
+    }
+
+    /**
+     * Crate a new connection
+     *
+     * @return the new created connection
+     * @throws SQLException
+     *             When fail to create a new connection.
+     */
+    private Connection createNewConnection() throws SQLException {
+        Connection conn = null;
+        conn = DriverManager.getConnection(databaseUrl, userName, password);
+        return conn;
+    }
+
+    /**
+     * Get a connection from the pool. If there is no free connection, return
+     * null
+     *
+     * @return the connection.
+     */
+    private Connection getConnectionFromPool() {
+        Connection conn = null;
+        if (freePool.size() > 0) {
+            conn = freePool.pop();
+            occupiedPool.add(conn);
+        }
+        return conn;
+    }
+
+    /**
+     * Make sure the connection is available now. Otherwise, reconnect it.
+     *
+     * @param conn
+     *            The connection for verification.
+     * @return the available connection.
+     * @throws SQLException
+     *             Fail to get an available connection
+     */
+    private Connection makeAvailable(Connection conn) throws SQLException {
+        if (isConnectionAvailable(conn)) {
+            return conn;
+        }
+
+        // If the connection is't available, reconnect it.
+        occupiedPool.remove(conn);
+        connNum--;
+        conn.close();
+
+        conn = createNewConnection();
+        occupiedPool.add(conn);
+        connNum++;
+        return conn;
+    }
+
+    /**
+     * By running a sql to verify if the connection is available
+     *
+     * @param conn
+     *            The connection for verification
+     * @return if the connection is available for now.
+     */
+    private boolean isConnectionAvailable(Connection conn) {
+        try (Statement st = conn.createStatement()) {
+            st.executeQuery(SQL_VERIFYCONN);
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    // Just an Example
+    public static void main(String[] args) throws SQLException {
+        Connection conn = null;
+        ConnectionManager pool = new ConnectionManager (
+                "jdbc:mysql://localhost:3306/mysql?serverTimezone=CET",
+                "root", "Passw0rd", 2);
         try {
-            // STEP 2: Register JDBC driver
-            Class.forName("com.mysql.jdbc.Driver");
+            conn = pool.getConnection();
+            try (Statement statement = conn.createStatement())
+            {
+                ResultSet res = statement.executeQuery("show tables");
+                System.out.println("There are below tables:");
+                while (res.next()) {
+                    String tblName = res.getString(1);
+                    System.out.println(tblName);
+                }
+            }
+        }
+        finally {
+            if (conn != null) {
+                pool.returnConnection(conn);
+            }
+        }
 
-            // STEP 3: Open a connection
-            System.out.println("Connecting to database No.1...");
-            conn = DriverManager.getConnection(DB_URL, USR, PSW);
-
-            /*// STEP 4: Execute a query
-            System.out.println("Creating database...");
-            stmt = conn.createStatement();
-
-            String sql = "CREATE DATABASE BLAH"; // BLAH = DB
-            stmt.executeUpdate(sql);
-            System.out.println("Database created successfully...");*/
-        } catch (SQLException se) {
-            // Handle errors for JDBC
-            se.printStackTrace();
-        } catch (Exception e) {
-            // Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            // Finally block used to close resources
-            try {
-                if(stmt != null)
-                    stmt.close();
-                    System.out.println("stmt close...");
-            } catch (SQLException se2) {
-            }// Nothing we can do
-            try {
-                if(conn != null)
-                    conn.close();
-                    System.out.println("conn close...");
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }// End finally try
-        }// End try
-        System.out.println("Connection to database No.1 tested");
-
-        // S2
-        Connection conn2 = null;
-        Statement stmt2 = null;
-        try {
-            // Register JDBC driver
-            Class.forName("com.mysql.jdbc.Driver");
-
-            // STEP 3: Open a connection
-            System.out.println("Connecting to database No.2...");
-            conn = DriverManager.getConnection(DB_URL_S2, USR_S2, PSW_S2);
-
-            /*// STEP 4: Execute a query
-            System.out.println("Creating database...");
-            stmt = conn.createStatement();
-
-            String sql = "CREATE DATABASE BLAH"; // BLAH = DB
-            stmt.executeUpdate(sql);
-            System.out.println("Database created successfully...");*/
-        } catch (SQLException se) {
-            // Handle errors for JDBC
-            se.printStackTrace();
-        } catch (Exception e) {
-            // Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            // Finally block used to close resources
-            try {
-                if(stmt2 != null)
-                    stmt2.close();
-                System.out.println("stmt2 close...");
-            } catch (SQLException se2) {
-            }// Nothing we can do
-            try {
-                if(conn2 != null)
-                    conn2.close();
-                System.out.println("conn2 close...");
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }// End finally try
-        }// End try
-        System.out.println("Connection to database No.2 tested");
-    }// End main
+    }
 
 
-}// End ConnectionManager
+}
